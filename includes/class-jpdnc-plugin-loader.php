0ce4996e1ca084188d79ba0a120bdae534d589f1
Phil O'Sullivan <philosullivan@users.noreply.github.com>
2026-03-02 12:31:06 -0500

    updates

--- includes/class-jpdnc-plugin-loader.php
+++ includes/class-jpdnc-plugin-loader.php
@@ -21,47 +21,39 @@
  * @subpackage Jpdnc_Plugin/includes
  * @author     JPDNC <https://jpdnc.org>
  */
+/**
+ * Register all actions and filters for the plugin.
+ */
+/**
+ * Register all actions and filters for the plugin.
+ */
 class Jpdnc_Plugin_Loader {
 
 	/**
 	 * The array of actions registered with WordPress.
 	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      array    $actions    The actions registered with WordPress to run when the filter is fired.
+	 * @var array $actions The actions registered with WordPress to run when the loader is run.
 	 */
-	protected $actions;
+	protected $actions = [];
 
 	/**
 	 * The array of filters registered with WordPress.
 	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      array    $filters    The filters registered with WordPress to run when the filter is fired.
+	 * @var array $filters The filters registered with WordPress to run when the loader is run.
 	 */
-	protected $filters;
+	protected $filters = [];
 
 	/**
 	 * Initialize the collections used to maintain the actions and filters.
-	 *
-	 * @since    1.0.0
 	 */
 	public function __construct() {
-
-		$this->actions = array();
-		$this->filters = array();
-
+		// Collections initialized as empty arrays via protected properties.
 	}
 
 	/**
 	 * Add a new action to the collection to be registered with WordPress.
 	 *
-	 * @since    1.0.0
-	 * @param    string               $hook             The name of the WordPress action that is being registered.
-	 * @param    object               $component        A reference to the instance of the object on which the callback is defined.
-	 * @param    string               $callback         The name of the function definition on the $component.
-	 * @param    int                  $priority         Optional. The priority at which the function should be fired. Default is 10.
-	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+	 * @access public
 	 */
 	public function add_action( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
@@ -70,12 +62,7 @@ class Jpdnc_Plugin_Loader {
 	/**
 	 * Add a new filter to the collection to be registered with WordPress.
 	 *
-	 * @since    1.0.0
-	 * @param    string               $hook             The name of the WordPress filter that is being registered.
-	 * @param    object               $component        A reference to the instance of the object on which the callback is defined.
-	 * @param    string               $callback         The name of the function definition on the $component.
-	 * @param    int                  $priority         Optional. The priority at which the function should be fired. Default is 10.
-	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+	 * @access public
 	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
@@ -84,45 +71,32 @@ class Jpdnc_Plugin_Loader {
 	/**
 	 * A utility function that is used to register the actions and filters into a single collection.
 	 *
-	 * @since    1.0.0
-	 * @access   public
-	 * @param    array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
-	 * @param    string               $hook             The name of the WordPress filter that is being registered.
-	 * @param    object               $component        A reference to the instance of the object on which the callback is defined.
-	 * @param    string               $callback         The name of the function definition on the $component.
-	 * @param    int                  $priority         The priority at which the function should be fired.
-	 * @param    int                  $accepted_args    The number of arguments that should be passed to the $callback.
-	 * @return   array                                  The collection of hooks that is being registered.
+	 * @access public
 	 */
 	public function add( $hooks, $hook, $component, $callback, $priority, $accepted_args ) {
-
-		$hooks[] = array(
+		$hooks[] = [
 			'hook'          => $hook,
 			'component'     => $component,
 			'callback'      => $callback,
 			'priority'      => $priority,
-			'accepted_args' => $accepted_args
-		);
+			'accepted_args' => $accepted_args,
+		];
 
 		return $hooks;
-
 	}
 
 	/**
-	 * Register the filters and actions with WordPress.
+	 * Execute the registration of all of the hooks with WordPress.
 	 *
-	 * @since    1.0.0
+	 * @access public
 	 */
 	public function run() {
-
 		foreach ( $this->filters as $hook ) {
-			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_filter( $hook['hook'], [ $hook['component'], $hook['callback'] ], $hook['priority'], $hook['accepted_args'] );
 		}
 
 		foreach ( $this->actions as $hook ) {
-			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_action( $hook['hook'], [ $hook['component'], $hook['callback'] ], $hook['priority'], $hook['accepted_args'] );
 		}
-
 	}
-
 }
